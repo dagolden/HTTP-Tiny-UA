0c632fd1ec34ddc1670d2f22c6bf90be7b8af626
David Golden <dagolden@cpan.org>
2013-10-05 08:40:38 -0400

    contributing note

--- lib/HTTP/Tiny/UA.pm
+++ lib/HTTP/Tiny/UA.pm
@@ -41,6 +41,19 @@ sub request {
 
 This module extends L<HTTP::Tiny> with higher-level convenience features.
 
+=head2 CONTRIBUTING
+
+Unlike L<HTTP::Tiny>, this module is open to additional features.  Please
+discuss new ideas on the bug tracker for feedback before implementing.
+
+While this module is not strictly "Tiny", here are some general guidelines:
+
+=for :list
+* The goal for this module is not feature/API equivalence with L<LWP::UserAgent>
+* Core module dependencies and "::Tiny" module dependencies are OK
+* Other CPAN modules should be used sparingly and only for good reasons
+* Any XS dependencies must be optional
+
 =head1 SEE ALSO
 
 =for :list
